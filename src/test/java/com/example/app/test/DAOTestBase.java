a61f7d553074f0004bf63918a0cfc7903a040f3f
oliver.eivak <oliver.eivak@gmail.com>
2017-03-18 14:31:27 +0200

    Optimize DAO tests
    This would make a bigger difference in large projects

--- src/test/java/com/example/app/test/DAOTestBase.java
+++ src/test/java/com/example/app/test/DAOTestBase.java
@@ -4,13 +4,37 @@ import static org.gwizard.hibernate.EM.em;
 
 import javax.persistence.EntityTransaction;
 
+import com.example.app.guice.GuiceInjector;
+import com.example.app.guice.module.ApplicationModule;
+import com.example.app.guice.module.TestModule;
+import com.google.inject.Guice;
+import com.google.inject.util.Modules;
+
+import org.gwizard.hibernate.HibernateModule;
+import org.gwizard.logging.LoggingModule;
 import org.junit.After;
 import org.junit.Before;
 
 public class DAOTestBase extends TestBase {
 
     @Before
-    public void beginTransaction() {
+    @Override
+    public void initializeTestBase() {
+        injector = Guice.createInjector(
+                Modules.override(
+                        new LoggingModule(),
+                        new HibernateModule(),
+                        new ApplicationModule() {
+                            @Override
+                            protected void configure() {
+                                // not needed for DAO tests
+                            }
+                        },
+                        new TestModule())
+                        .with(overrideModule()));
+
+        GuiceInjector.setInjector(injector);
+
         em().getTransaction().begin();
     }
 
